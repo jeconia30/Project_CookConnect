b46705c45cd37b9b0b79e9ac0ccab29e611b377f
Adil Busra Muhammad <muhammadadil@students.usu.ac.id>
2025-12-09 15:46:31 +0700

    plis bisa

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Gunakan Env Var untuk Vercel
 const BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api";
+  import.meta.env.VITE_API_BASE_URL;
 
 const api = axios.create({
   baseURL: BASE_URL,
